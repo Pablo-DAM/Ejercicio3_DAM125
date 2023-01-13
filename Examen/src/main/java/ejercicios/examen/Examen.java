f9fb6f10cd05f9d92b416534e81baa0396732482
Pablo-DAM <pablo.sainzluque@iesmiguelherrero.com>
2023-01-13 17:47:37 +0100

    commit

---
+++ Examen/src/main/java/ejercicios/examen/Examen.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ejercicios.examen;
+
+/**
+ *
+ * @author DAM125
+ */
+public class Examen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
